83ecd54ba2f79465c77bca47884385a82396a0e0
보경 <qkrqhru@knu.ac.kr>
2024-03-14 15:27:43 +0900

    프로젝트 초기 세팅

---
+++ I-Catch/I-Catch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I-Catch
+//
+//  Created by Bokyung on 3/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("I-Catch")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ I-Catch/I-Catch/I_CatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_CatchApp.swift
+//  I-Catch
+//
+//  Created by Bokyung on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct I_CatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
